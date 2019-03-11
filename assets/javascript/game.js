6845bda7d54770857eda5e7488aeb9ceefa7b504
Annie Pham <anniekpham@yahoo.com>
2019-03-10 23:52:53 -0700

    fixed a function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,37 @@
 let allied, 
     enemy,
-    isdoneSelect,
+    isdoneSelect = false,
     attacklog
+let characters = {  //charcters[0].imgUrl
+    'Harry': {
+        name: 'Harry Potter',
+        health: 100,
+        attack: 15,
+        countattact: 8,
+        imgUrl: '../images/harry.jp'
+    },
+    'Snape': {
+        name: 'Professor Snape',
+        health: 120,
+        attack: 13,
+        countattact: 9,
+        imgUrl: '../images/Snape.jp'
+    },
+    'Voldemort': {
+        name: 'Lord Voldemort',
+        health: 150,
+        attack: 16,
+        countattact: 11,
+        imgUrl: '../images/Voldemort.jpeg'
+    },
+    'Bellatrix': {
+        name: 'Bellatrix Lestrange',
+        health: 130,
+        attack: 11,
+        countattact: 10,
+        imgUrl: '../images/Bellatrix.jp'
+    }
+}
 
 const reset = () => {
     allied = ''
@@ -13,7 +43,7 @@ const reset = () => {
 
 const attack = () => {
     let attack = document.createElement('button')
-    attack.textcontent = 'Attack!'
+    attack.textContent = 'Attack!'
     attack.className ='attack'
 
     document.querySelector('#attack').append(attack)
@@ -30,15 +60,24 @@ const attackpower = () => {
 
 document.addEventListener ('click', ({ target }) => {
     if (target.className === 'character' && !allied) {
+
         document.querySelector('#actioncontainer').style.visibility = ''
+
         allied = target.dataset.value
+
         document.querySelector('#yourchar').textContent = allied
+
         document.querySelector('#character').disabled = true;
+
     } else {
         if (target.className === 'character' && !isdoneSelect){
+
         enemy = target.dataset.value
+
         document.querySelector('#enemychar').textContent = enemy
+        
         // document.querySelector('#character').disabled = true;
+
         isdoneSelect = true
         attack(attackpower())
         }
@@ -46,3 +85,17 @@ document.addEventListener ('click', ({ target }) => {
 })
 
 reset()
+
+
+
+// //example loop
+// for(var i = 0; i<= characters.length; i++){
+//     let html = 
+    
+//     <div>
+//     <p>`${characters[i].name}`</p>
+    
+//     </div>
+    
+//     //push this into the html each time
+// }
