99fbac964f8600007b1f794291c7f6f4b6b1a441
anniekpham <anniekpham@yahoo.com>
2019-03-12 00:59:47 -0700

    finished 85% of the game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,138 +1,112 @@
-let allied,
+let alliescounter,
     allieshealth,
     alliesattack,
-    enemy,
+    enemyscounter,
     enemyshealth,
-    enemysattack,
-    attackbtn,
+    attackbtn = 0,
     isdoneSelect = false,
     attacklog
-// let characters = {  //charcters[0].imgUrl
-//     'Harry': {
-//         name: 'Harry Potter',
-//         health: 100,
-//         attack: 15,
-//         countattact: 8,
-//         imgUrl: 'harry.jpg'
-//     },
-//     'Snape': {
-//         name: 'Professor Snape',
-//         health: 120,
-//         attack: 13,
-//         countattact: 9,
-//         imgUrl: '../images/Snape.jpg'
-//     },
-//     'Voldemort': {
-//         name: 'Lord Voldemort',
-//         health: 150,
-//         attack: 16,
-//         countattact: 11,
-//         imgUrl: '../images/Voldemort.jpeg'
-//     },
-//     'Bellatrix': {
-//         name: 'Bellatrix Lestrange',
-//         health: 130,
-//         attack: 11,
-//         countattact: 10,
-//         imgUrl: '../images/Bellatrix.jpg'
-//     }
-// }
 
-//example loop
-// for(var i = 0; i<= characters.length; i++){
-//     let html = 
-    
-//     <div>
-//     <p>`${characters[i].name}`</p>
-    
-//     </div>
-    
-    //push this into the html each time
-// }
 let audio = new Audio('Hedwig.mp3')
 audio.loop = true
 
-const reset = () => {
-    allied = ''
+const gamestart = () => {
+    alliescounter = ''
     enemy = ''
     isdoneSelect = false
     document.querySelector('#attack').innerHTML = ''
-    document.querySelector('#actioncontainer').style.visibility = 'hidden'
-}
-
-const attack = () => {
-    let attack = document.createElement('button')
-    attack.textContent = 'Attack!'
-    attack.className ='attack'
-
-    document.querySelector('#attack').append(attack)
-
 }
 
 const newenemy = () => {
     if (enemyshealth <= attackbtn) {
         enemyshealth = 0
+        enemyscounter = 0
         document.querySelector('#enemychar').innerHTML = enemyshealth
         if (enemyshealth === 0){
-            document.querySelector('#enemychar').innerHTML = ''
-            enemy = ''
-            enemysattack = ''
-            enemyshealth = ''
             !isdoneSelect
-            document.querySelector('#attack').textContent = ''
+            document.addEventListener ('click', ({ target }) => {
+                if (target.className === 'character') {
+                    enemyscounter = target.dataset.counter
+                    enemyshealth = target.dataset.health
+                    document.querySelector('#enemychar').innerHTML = enemyshealth
+                    target.style.border = '10px solid rgb(153, 0, 0)'
+                    target.classList.remove('character');
+                }
+            })
+            isclass = document.getElementsByClassName('character');
+            if (isclass.length > 0) {
+                
+            } else {
+                document.querySelector('#attack').innerHTML = ''
+                let won = document.createElement('p')
+                won.innerHTML ='YOU WON!'
+                won.className = 'result'
+                document.querySelector('#actioncontainer').append(won)
+            }
+        }
+    }
+}
+
+const gameover = () => {
+    if(allieshealth <= enemyscounter) {
+        allieshealth = 0
+        document.querySelector('#yourchar').innerHTML = allieshealth
+        alliesattack = 0
+        document.querySelector('#attack').innerHTML = ''
+        let lost = document.createElement('p')
+        lost.innerHTML =`You've been defeated!`
+        lost.className = 'result'
+        document.querySelector('#actioncontainer').append(lost)
+        document.addEventListener("click",handler,true);
+        function handler(e){
+            e.stopPropagation();
+            e.preventDefault();
         }
     }
 }
 
 const attackpower = () => {
+    let attack = document.createElement('button')
+    attack.textContent = 'Expelliarmus!'
+    attack.className ='attack'
+
+    document.querySelector('#attack').append(attack)
+
     document.addEventListener('click', ({target}) => {
         if (target.className === 'attack') {
             attackbtn = alliesattack
+            alliesattack += parseInt(attackbtn)
             enemyshealth -= attackbtn
-            allieshealth -= enemy.counter
+            allieshealth -= enemyscounter
             document.querySelector('#yourchar').innerHTML = allieshealth
             document.querySelector('#enemychar').innerHTML = enemyshealth
-            alliesattack += parseInt(attackbtn)
-            console.log(attackbtn)
         }
-        // newenemy()
+        newenemy()
+        gameover()
     })
 }
 
 document.addEventListener ('click', ({ target }) => {
-    if (target.className === 'character' && !allied) {
+    if (target.className === 'character' && !alliescounter) {
         audio.play()
-
-        document.querySelector('#actioncontainer').style.visibility = ''
-
-        allied = target.dataset
-
+        document.querySelector('.intro').innerHTML = ''
+        alliescounter = target.dataset.counter
         allieshealth = target.dataset.health
-
-        alliesattack = target.dataset.attack
-
-        document.querySelector('#yourchar').innerHTML = allieshealth
-
-        document.querySelector('#character').disabled = true;
-
+        alliesattack = parseInt(target.dataset.attack)
+        document.querySelector('#yourchar').textContent = allieshealth
+        target.classList.remove('character')
+        target.style.border = '10px solid rgb(42, 152, 255)';
     } else {
         if (target.className === 'character' && !isdoneSelect){
-
-        enemy = target.dataset
-
+        enemyscounter = target.dataset.counter
         enemyshealth = target.dataset.health
-
-        enemysattack = target.dataset.attack
-
         document.querySelector('#enemychar').innerHTML = enemyshealth
-        
-        // document.querySelector('#character').disabled = true;
-
+        target.classList.remove('character');
+        target.style.border = '10px solid rgb(153, 0, 0)'
         isdoneSelect = true
-        attack()
         attackpower()
         }
     }
 })
 
-reset()
+gamestart()
